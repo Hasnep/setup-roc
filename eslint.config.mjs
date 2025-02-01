63b30b7a5fc2be7bd79491fc8887e0d37ad75227
Hannes <h@nnes.dev>
2025-02-01 22:24:29 +0800

    Fix Typescript pre-commit checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  { ignores: ["dist/*"] }
+  { ignores: ["dist/*"] },
 );
